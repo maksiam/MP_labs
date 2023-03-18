29e0d2815fc2b9ad40c3af9b04ff34d77512c8c0
root <root@LAPTOP-1ROROS9Q.localdomain>
2023-03-18 11:44:10 +0300

    Done

--- mutexes.cpp
+++ mutexes.cpp
@@ -1,4 +1,6 @@
 #include "mutexes.h"
+#include <stack>
+#include <mutex>
 #include <signal.h>
 #include <chrono>
 #include <csignal>
@@ -18,7 +20,15 @@ Useful links:
 static variables: https://www.geeksforgeeks.org/static-variables-in-c/
 pthread mutex: https://docs.oracle.com/cd/E26502_01/html/E35303/sync-110.html
 */
-void thread_safe_increment(int& value) { UNUSED(value); }
+
+void thread_safe_increment(int& value) {
+  static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_mutex_lock(&mutex); // acquire the lock
+
+  value++; // increment the value
+
+  pthread_mutex_unlock(&mutex); // release the lock
+}
 
 /*
 Task 2. Implement the transfer of the value from the provide function to the
@@ -39,10 +49,15 @@ Steps (provide):
 4. Send a notification of readiness via pthread_cond_signal
 5. Unlock the mutex from ctx.mtx
 */
+
 void provide(context_t& ctx, int value) {
-  UNUSED(ctx);
-  UNUSED(value);
+  pthread_mutex_lock(&ctx.mtx); // lock the mutex
+  ctx.shared_value = value; // set the value
+  ctx.is_provided = true; // set the readiness flag
+  pthread_cond_signal(&ctx.cond); // send notification
+  pthread_mutex_unlock(&ctx.mtx); // unlock the mutex
 }
+
 
 /*
 Steps (consume):
@@ -52,10 +67,17 @@ pthread_cond_wait to wait
 3. Unlock the mutex from ctx.mtx
 4. Return the ctx.shared_value value from the function
 */
+
 int consume(context_t& ctx) {
-  UNUSED(ctx);
-  return 0;
+  pthread_mutex_lock(&ctx.mtx); // lock the mutex
+  while (!ctx.is_provided) {
+    pthread_cond_wait(&ctx.cond, &ctx.mtx); // wait for notification
+  }
+  int value = ctx.shared_value; // get the value
+  pthread_mutex_unlock(&ctx.mtx); // unlock the mutex
+  return value;
 }
+
 
 /*
 Task 3. Implement a thread-safe stack.
@@ -69,12 +91,26 @@ pthread mutex: https://docs.oracle.com/cd/E26502_01/html/E35303/sync-110.html
 /*
 push saves value to the top of the stack
 */
-void stack::push(int value) { UNUSED(value); }
+void stack::push(int value) { 
+    pthread_mutex_lock(&m_mutex);
+    m_stack.push_back(value);
+    pthread_mutex_unlock(&m_mutex); 
+}
 
 /*
 pop removes a value from the top of the stack and returns it from the function
 */
-int stack::pop() { return 0; }
+int stack::pop() {
+    pthread_mutex_lock(&m_mutex);
+    if (m_stack.empty()) {
+      pthread_mutex_unlock(&m_mutex);
+      throw std::out_of_range("Stack is empty");
+    }
+    int value = m_stack.back();
+    m_stack.pop_back();
+    pthread_mutex_unlock(&m_mutex);
+    return value;
+}
 
 /*
 Task 4. Implement a multithreaded merge sort that will outperform a
--- mutexes.h
+++ mutexes.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdexcept>
 #include <pthread.h>
 #include <cstdint>
 #include <vector>
@@ -21,19 +22,22 @@ int consume(context_t& ctx);
 
 // 3. Task
 class stack {
-  /*
-  Add here any fields that are needed for implementation.
-  Recommended fields are pthread_mutex_t and std::vector<int>
+public:
+  stack() {
+    pthread_mutex_init(&m_mutex, nullptr);
+  }
 
-  Useful links:
-  std::vector: https://en.cppreference.com/w/cpp/container/vector
-  pthread mutex: https://docs.oracle.com/cd/E26502_01/html/E35303/sync-110.html
-  */
+  ~stack() {
+    pthread_mutex_destroy(&m_mutex);
+  }
 
- public:
   void push(int value);
 
   int pop();
+
+private:
+  std::vector<int> m_stack;
+  pthread_mutex_t m_mutex;
 };
 
 // 4. Task
